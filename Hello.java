a3e0b95787f340a4c73b3c4c21c8dcd7708c83b8
unknown <you@example.com>
2019-10-23 11:23:06 +0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,23 @@
+package com.loctek;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("78900000");
+		System.out.print("78900000");
+		System.out.print("78900000");
+		System.out.print("78900000");
+		
+		
+		System.out.print("78900000");
+		System.out.print("78900000");
+		System.out.print("78900000");
+		
+		
+	}
+
+}
